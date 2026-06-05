499a741c17cc6bb0810a205855046e281591f467
Bushman <bushmanella@gmail.com>
2026-06-05 15:47:00 -0500

    Fixed the class tests so they pass

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -11,13 +11,13 @@ test_atoms = [
     classes.Atom("H", [1, 0, 0]),
     classes.Atom("O", [0, 0, 0]),
 ]
-test_bonds = [[0, 2, "s"], [1, 2, "s"]]
+test_bonds = [[0, 2], [1, 2]]
 test_mol = classes.Mol(test_atoms, test_bonds)
 test_mol2 = classes.Mol(test_atoms, test_bonds)
 
 assert test_mol == test_mol2
 
-assert test_mol._bonds == [[0, 2, "s"], [1, 2, "s"]]
+assert test_mol._bonds == [[0, 2], [1, 2]]
 assert test_mol.atoms == [
     classes.Atom("H", [0, 1, 0]),
     classes.Atom("H", [1, 0, 0]),
@@ -26,17 +26,17 @@ assert test_mol.atoms == [
 
 assert (
     repr(test_mol) == "Mol([Atom('H', [0, 1, 0]), Atom('H', [1, 0, 0]), "
-    "Atom('O', [0, 0, 0])], [[0, 2, 's'], [1, 2, 's']])"
+    "Atom('O', [0, 0, 0])], [[0, 2], [1, 2]])"
 )
 
-assert test_mol.is_bond(0, 1) == "n"
-assert test_mol.is_bond(2, 0) == "s"
+assert not test_mol.is_bond(0, 1)
+assert test_mol.is_bond(2, 0)
 
 test_mol.del_bond(0, 2)
-assert test_mol.is_bond(0, 2) == "n"
+assert not test_mol.is_bond(0, 2)
 
-test_mol.add_bond(0, 2, "d")
-assert test_mol.is_bond(0, 2) == "d"
+test_mol.add_bond(0, 1)
+assert test_mol.is_bond(0, 1)
 
 assert test_mol.dist(0, 2) == test_mol.dist(1, 2) == 1
 assert test_mol.dist(0, 1) == 2**0.5
