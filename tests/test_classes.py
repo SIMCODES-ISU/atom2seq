1d12c094665f930c1c13547ac8725fb1a7d2954e
Aspen AMM <aspen.am.meissner@gmail.com>
2026-06-11 16:21:43 -0500

    restructured test_classes to be pytest-compliant

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,42 +1,93 @@
-from atom2seq import classes
+from atom2seq.classes import Atom, Mol
+import pytest
 
-test_atom = classes.Atom("H", [0, 0, 0])
 
-assert test_atom.symbol == "H"
-assert test_atom.coords == [0, 0, 0]
-assert repr(test_atom) == "Atom('H', [0, 0, 0])"
+@pytest.fixture
+def hydrogen():
+    return ("H", (0, 0, 0))
 
-test_atoms = [
-    classes.Atom("H", [0, 1, 0]),
-    classes.Atom("H", [1, 0, 0]),
-    classes.Atom("O", [0, 0, 0]),
-]
-test_bonds = [[0, 2], [1, 2]]
-test_mol = classes.Mol(test_atoms, test_bonds)
-test_mol2 = classes.Mol(test_atoms, test_bonds)
 
-assert test_mol == test_mol2
+def test_symbol(hydrogen):
+    test_atom = Atom(*hydrogen)
+    assert test_atom.symbol == "H"
 
-assert test_mol._bonds == [[0, 2], [1, 2]]
-assert test_mol.atoms == [
-    classes.Atom("H", [0, 1, 0]),
-    classes.Atom("H", [1, 0, 0]),
-    classes.Atom("O", [0, 0, 0]),
-]
 
-assert (
-    repr(test_mol) == "Mol([Atom('H', [0, 1, 0]), Atom('H', [1, 0, 0]), "
-    "Atom('O', [0, 0, 0])], [[0, 2], [1, 2]])"
-)
+def test_coords(hydrogen):
+    test_atom = Atom(*hydrogen)
+    assert test_atom.coords == (0, 0, 0)
 
-assert not test_mol.is_bond(0, 1)
-assert test_mol.is_bond(2, 0)
 
-test_mol.del_bond(0, 2)
-assert not test_mol.is_bond(0, 2)
+def test_atom_repr(hydrogen):
+    test_atom = Atom(*hydrogen)
+    assert repr(test_atom) == "Atom('H', (0, 0, 0))"
 
-test_mol.add_bond(0, 1)
-assert test_mol.is_bond(0, 1)
 
-assert test_mol.dist(0, 2) == test_mol.dist(1, 2) == 1
-assert test_mol.dist(0, 1) == 2**0.5
+@pytest.fixture
+def water():
+    test_atoms = [
+        Atom("H", [0, 1, 0]),
+        Atom("H", [1, 0, 0]),
+        Atom("O", [0, 0, 0]),
+    ]
+    test_bonds = [[0, 2], [1, 2]]
+    return (test_atoms, test_bonds)
+
+
+def test_mol_eq(water):
+    test_mol = Mol(*water)
+    test_mol2 = Mol(*water)
+    assert test_mol == test_mol2
+
+
+def test_get_bonds(water):
+    test_mol = Mol(*water)
+    assert test_mol.get_bonds() == [[0, 2], [1, 2]]
+
+
+def test_atoms(water):
+    test_mol = Mol(*water)
+    assert test_mol.atoms == [
+        Atom("H", [0, 1, 0]),
+        Atom("H", [1, 0, 0]),
+        Atom("O", [0, 0, 0]),
+    ]
+
+
+def test_mol_repr(water):
+    test_mol = Mol(*water)
+    assert (
+        repr(test_mol) == "Mol([Atom('H', [0, 1, 0]), Atom('H', [1, 0, 0]), "
+        "Atom('O', [0, 0, 0])], [[0, 2], [1, 2]])"
+    )
+
+
+def test_not_is_bond(water):
+    test_mol = Mol(*water)
+    assert not test_mol.is_bond(0, 1)
+
+
+def test_is_bond(water):
+    test_mol = Mol(*water)
+    assert test_mol.is_bond(2, 0)
+
+
+def test_del_bond(water):
+    test_mol = Mol(*water)
+    test_mol.del_bond(0, 2)
+    assert not test_mol.is_bond(0, 2)
+
+
+def test_add_bond(water):
+    test_mol = Mol(*water)
+    test_mol.add_bond(0, 1)
+    assert test_mol.is_bond(0, 1)
+
+
+def test_dist_1(water):
+    test_mol = Mol(*water)
+    assert test_mol.dist(0, 2) == test_mol.dist(1, 2) == 1
+
+
+def test_dist_sqrt2(water):
+    test_mol = Mol(*water)
+    assert test_mol.dist(0, 1) == pytest.approx(2**0.5)
