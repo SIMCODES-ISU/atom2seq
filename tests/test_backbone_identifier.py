60bcff5042e7c93da8bcfe0eaae71a2206fc596d
Aspen AMM <aspen.am.meissner@gmail.com>
2026-06-16 14:02:56 -0500

    made the ider work!!!!!!!

--- tests/test_backbone_identifier.py
+++ tests/test_backbone_identifier.py
@@ -1,6 +1,6 @@
 import pytest
 
-from atom2seq import backbone_identifer as bid
+from atom2seq import backbone_identifier as bid
 from atom2seq.parsers import parse_xyz
 
 prefix = __file__.removesuffix("test_backbone_identifier.py")
@@ -23,6 +23,10 @@ def glycine():
 
 def test_id_n_term_G(glycine):
     assert bid.id_n_term(glycine) == [7]
+
+
+def test_id_pro_n_term_G(glycine):
+    assert bid.id_pro_n_term(glycine) == []
 
 
 def test_find_atom_G(glycine):
@@ -61,85 +65,68 @@ def test_label_backbone_G(glycine):
 
 
 @pytest.fixture
-def gay():
-    glyAlaTyr = parse_xyz(prefix + "assets/backbone_identifier_tests/gay.xyz")
-    glyAlaTyr.add_bond(0, 1)
-    glyAlaTyr.add_bond(1, 2)
-    glyAlaTyr.add_bond(1, 4)
-    glyAlaTyr.add_bond(3, 4)
-    glyAlaTyr.add_bond(4, 5)
-    glyAlaTyr.add_bond(4, 6)
-    glyAlaTyr.add_bond(6, 7)
-    glyAlaTyr.add_bond(6, 8)
-    glyAlaTyr.add_bond(8, 11)
-    glyAlaTyr.add_bond(9, 10)
-    glyAlaTyr.add_bond(10, 13)
-    glyAlaTyr.add_bond(11, 12)
-    glyAlaTyr.add_bond(12, 13)
-    glyAlaTyr.add_bond(12, 16)
-    glyAlaTyr.add_bond(13, 14)
-    glyAlaTyr.add_bond(13, 15)
-    glyAlaTyr.add_bond(16, 17)
-    glyAlaTyr.add_bond(16, 21)
-    glyAlaTyr.add_bond(18, 23)
-    glyAlaTyr.add_bond(19, 20)
-    glyAlaTyr.add_bond(20, 25)
-    glyAlaTyr.add_bond(21, 26)
-    glyAlaTyr.add_bond(22, 27)
-    glyAlaTyr.add_bond(23, 24)
-    glyAlaTyr.add_bond(23, 28)
-    glyAlaTyr.add_bond(24, 29)
-    glyAlaTyr.add_bond(25, 26)
-    glyAlaTyr.add_bond(26, 27)
-    glyAlaTyr.add_bond(26, 35)
-    glyAlaTyr.add_bond(27, 28)
-    glyAlaTyr.add_bond(27, 32)
-    glyAlaTyr.add_bond(28, 33)
-    glyAlaTyr.add_bond(29, 30)
-    glyAlaTyr.add_bond(29, 34)
-    glyAlaTyr.add_bond(30, 31)
-    glyAlaTyr.add_bond(33, 34)
-    glyAlaTyr.add_bond(33, 37)
-    glyAlaTyr.add_bond(34, 38)
-    glyAlaTyr.add_bond(35, 36)
-    glyAlaTyr.add_bond(35, 39)
-    glyAlaTyr.add_bond(39, 40)
-    return glyAlaTyr
+def gas():
+    gas = parse_xyz(prefix + "assets/backbone_identifier_tests/gas.xyz")
+    gas.add_bond(0, 1)
+    gas.add_bond(1, 2)
+    gas.add_bond(1, 4)
+    gas.add_bond(3, 4)
+    gas.add_bond(4, 5)
+    gas.add_bond(4, 7)
+    gas.add_bond(6, 7)
+    gas.add_bond(7, 9)
+    gas.add_bond(8, 9)
+    gas.add_bond(9, 12)
+    gas.add_bond(10, 13)
+    gas.add_bond(11, 12)
+    gas.add_bond(12, 13)
+    gas.add_bond(12, 16)
+    gas.add_bond(13, 14)
+    gas.add_bond(13, 17)
+    gas.add_bond(15, 16)
+    gas.add_bond(16, 19)
+    gas.add_bond(18, 19)
+    gas.add_bond(19, 22)
+    gas.add_bond(20, 23)
+    gas.add_bond(21, 22)
+    gas.add_bond(22, 23)
+    gas.add_bond(22, 27)
+    gas.add_bond(23, 24)
+    gas.add_bond(23, 28)
+    gas.add_bond(24, 25)
+    gas.add_bond(26, 27)
+    gas.add_bond(27, 29)
+    gas.add_bond(29, 30)
+    return gas
 
 
-def test_id_n_term_GAY(gay):
-    assert bid.id_n_term(gay) == [1]
+def test_id_n_term_GAS(gas):
+    print(gas)
+    assert bid.id_n_term(gas) == [1]
 
 
-def test_find_atom_GAY(gay):
-    assert bid.find_atom(gay, [1], "C") == [4]
+def test_find_atom_GAS(gas):
+    assert bid.find_atom(gas, [1], "C") == [4]
 
 
-def test_small_backbone_iter_GAY(gay):
-    assert bid.small_backbone_iter(gay, [[1]], "C") == [[1, 4]]
+def test_small_backbone_iter_GAS(gas):
+    assert bid.small_backbone_iter(gas, [[1]], "C") == [[1, 4]]
 
 
-def test_large_backbone_iter_GAY(gay):
-    assert bid.large_backbone_iter(gay, [[1]]) == (
-        [[1, 4, 6, 8]],
+def test_large_backbone_iter_GAS(gas):
+    assert bid.large_backbone_iter(gas, [[1]]) == (
+        [[1, 4, 7, 9]],
         False,
     )
 
 
-def test_find_backbone_GAY(gay):
-    assert bid.find_backbone(gay) == [1, 4, 6, 8, 11, 15, 19, 24, 33, 37]
+def test_find_backbone_GAS(gas):
+    assert bid.find_backbone(gas) == [1, 4, 7, 9, 12, 16, 19, 22, 27, 26]
 
 
-def test_label_backbone_GAY(gay):
-    bid.label_backbone(gay)
-    assert gay.get_backbone() == [
-        False,
-        True,
-        False,
-        False,
-        True,
-        False,
-        True,
+def test_label_backbone_GAS(gas):
+    bid.label_backbone(gas)
+    assert gas.get_backbone() == [
         False,
         True,
         False,
@@ -147,6 +134,10 @@ def test_label_backbone_GAY(gay):
         True,
         False,
         False,
+        True,
+        False,
+        True,
+        False,
         False,
         True,
         False,
@@ -155,15 +146,7 @@ def test_label_backbone_GAY(gay):
         True,
         False,
         False,
-        False,
-        False,
         True,
-        False,
-        False,
-        False,
-        False,
-        False,
-        False,
         False,
         False,
         True,
@@ -171,5 +154,8 @@ def test_label_backbone_GAY(gay):
         False,
         False,
         True,
+        True,
+        False,
+        False,
         False,
     ]
