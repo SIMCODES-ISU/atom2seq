ca862b1238d757373306a1709fdfd14402800164
Bushman <bushmanella@gmail.com>
2026-06-18 15:17:43 -0500

    Added Q.bean file that I forgot; ran the following AAs on Nova and put the xyz data into .nwc files!

--- tests/assets/format_fixer/bean_to_xyz.py
+++ tests/assets/format_fixer/bean_to_xyz.py
@@ -1,5 +1,5 @@
 with open(
-    r"C:\Users\eb528353\OneDrive - Loras College\Desktop\atom2seq\tests\assets\trimers\trimers_gjf_files\trivaline.bean"  # noqa
+    r"C:\Users\eb528353\OneDrive - Loras College\Desktop\atom2seq\tests\assets\monomers\monomers_gjf_files\glutamine.bean"  # noqa
 ) as f:
     content = f.read()
 cleaned_content = (
