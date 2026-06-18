6c57b8ef36c025d651442c3ed6d544fd04c86366
Aspen AMM <aspen.am.meissner@gmail.com>
2026-06-18 15:08:14 -0500

    added testing files to be filled with the nwc output

--- tests/test_side_chain_identifier.py
+++ tests/test_side_chain_identifier.py
@@ -1,20 +1,40 @@
-import pytest
-
 from atom2seq.backbone_identifier import label_backbone
 from atom2seq.bond_detector import bond_mol
-from atom2seq.parsers import parse_xyz
+from atom2seq.parsers import parse_nwc
 from atom2seq.side_chain_identifier import id_side_chains
 
 prefix = __file__.removesuffix("test_side_chain_identifier.py")
 
 
-@pytest.fixture
-def glycine():
-    gly = parse_xyz(prefix + "assets/side_chain_identifier_tests/glycine.xyz")
-    bond_mol(gly)
-    label_backbone(gly)
-    return gly
+aas = [
+    "A",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "K",
+    "L",
+    "M",
+    "N",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "V",
+    "W",
+    "Y",
+]
 
 
-def test_glycine(glycine):
-    assert id_side_chains(glycine) == ["G"]
+def test_aa_monomers():
+    for letter in aas:
+        mol = parse_nwc(
+            prefix + f"assets/side_chain_identifier_tests/mon{letter}.nwc"
+        )  # noqa
+        bond_mol(mol)
+        label_backbone(mol)
+        assert id_side_chains(mol) == [letter]
