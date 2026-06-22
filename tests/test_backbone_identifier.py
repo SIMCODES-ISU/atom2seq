32a2ae9ad26bf8bb57ab2d3e8ad630cf9902ff43
Aspen AMM <aspen.am.meissner@gmail.com>
2026-06-22 08:49:24 -0500

    removed the empty testing cases in test_backbone_identifier

--- tests/test_backbone_identifier.py
+++ tests/test_backbone_identifier.py
@@ -159,30 +159,3 @@ def test_label_backbone_GAS(gas):
         False,
         False,
     ]
-
-
-aas = [
-    "alanine",
-    "arginine",
-    "asparagine",
-    "aspartate",
-    "cysteine",
-    "glutamate",
-    "glycine",
-    "histidine",
-    "isoleucine",
-    "leucine",
-    "lysine",
-    "methionine",
-    "phenylalanine",
-    "proline",
-    "serine",
-    "threonine",
-    "tryptophan",
-    "tyrosine",
-    "valine",
-]
-
-
-def test_all_aas():
-    pass
